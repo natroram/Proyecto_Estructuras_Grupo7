494c02eac5abcc2f601ab0ba820d3414f9bc9e8f
Natalia Ramirez <Natalia Ramirez@Nats-Laptop>
2020-12-04 18:22:54 -0500

    corrigiendo main

--- Proyecto_Grupo7/src/main/java/com/mycompany/proyecto_grupo7/App.java
+++ Proyecto_Grupo7/src/main/java/com/mycompany/proyecto_grupo7/App.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import pantallas.VistaPantalla;
 
 /**
  * JavaFX App
@@ -15,9 +16,9 @@ public class App extends Application {
     public void start(Stage stage) {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
-
+        VistaPantalla vista = new VistaPantalla();
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        var scene = new Scene(vista.getRoot());
         stage.setScene(scene);
         stage.show();
     }
